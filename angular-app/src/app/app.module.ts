ea36d47168e61c3b80279acbbac11b5fbeb34ac3
Abdelrahman Hussein <abdelrahman.h.mahmoued@gmail.com>
2021-01-08 18:17:00 +0200

    feat: Migrating Angular Application to Single-spa

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { APP_BASE_HREF } from '@angular/common';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{provide: APP_BASE_HREF, useValue: '/'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
